9f2f2ef7b1486212f704046ec97a159e07403d3c
abdillahhamzahali <abdillahhamzahali@gmail.com>
2022-11-09 18:47:15 +0700

    tambah fitur

--- black.py
+++ black.py
@@ -1,3 +1,4 @@
+from unittest import result
 import pyttsx3
 import speech_recognition as sr
 import datetime
@@ -32,13 +33,16 @@ def wishMe():
         speak("Good Evening" + MASTER)
         speak("")
 
+# for index, name in enumerate(sr.Microphone.list_microphone_names()):
+#     print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
+
 # microphone
 def takeCommand():
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("listening...")
         audio = r.listen(source)
-    
+
     try:
         print("Recognizing..")
         query = r.recognize_google(audio, language="en-us")
@@ -55,3 +59,30 @@ speak("Hello my name is Black,i can help you")
 wishMe()
 query = takeCommand()
 
+# logic for tasks as per query
+if "wikipedia" in query.lower():
+    speak("searching in wikipedia")
+    query = query.replace("wikipedia", "")
+    results = wikipedia.summary(query, sentences=2)
+    print(results)
+    speak(results)
+
+elif "open youtube" in query.lower():
+    url = "youtube.com"
+    chrome_path = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s"
+    webbrowser.get(chrome_path).open_new(url)
+
+elif "open google" in query.lower():
+    url = "google.com"
+    chrome_path = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s"
+    webbrowser.get(chrome_path).open_new(url)
+
+# elif "play music" in query.lower():
+#     song_dir = "C:/Users/Windows 10/Music"
+#     songs = os.listdir(song_dir)
+#     print(songs)
+#     os.startfile(os.path.join(song_dir, songs[0]))
+
+elif "the time" in query.lower():
+    strTime = datetime.datetime.now().strftime("%H:%M:%S")
+    speak(f"{MASTER} the time is {strTime}")
